e4d7bbb39c25b4014057c2f3f3bad6aa1a72a659
Buvisan Admin <eticaretbuvisan@gmail.com>
2025-12-29 13:42:59 +0300

    Harita ikon fix

--- components/HaritaBileseni.tsx
+++ components/HaritaBileseni.tsx
@@ -4,6 +4,15 @@ import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
+// --- İKON SORUNU ÇÖZÜMÜ ---
+// Leaflet'in varsayılan ikon ayarlarını sıfırlıyoruz.
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
+
 // --- 1. YEŞİL İKON (Sorunsuz) ---
 const greenIcon = new L.Icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/markers/marker-icon-2x-green.png',
@@ -22,7 +31,7 @@ const redPulseIcon = new L.Icon({
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
     shadowSize: [41, 41],
-    className: 'yanip-sonen-pin' // CSS'deki animasyonu tetikler
+    className: 'yanip-sonen-pin' // CSS animasyonu için sınıf
 });
 
 function HaritaUcus({ konum }: { konum: [number, number] | null }) {
@@ -45,21 +54,21 @@ export default function HaritaBileseni({ vincler, secilenVinc, setSecilenVinc }:
         >
             <TileLayer
                 attribution='&copy; OpenStreetMap'
+                // Daha modern, sade harita teması
                 url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
             />
 
             <HaritaUcus konum={secilenVinc && secilenVinc.lat ? [secilenVinc.lat, secilenVinc.lng] : null} />
 
             {vincler.map((vinc: any) => {
-                // --- MANTIK: AKTİF ARIZA VAR MI? ---
-                // service_tickets dizisinde 'tamamlandi' OLMAYAN bir kayıt var mı?
+                // Arıza kontrolü: 'tamamlandi' olmayan bir kayıt var mı?
                 const arizaVar = vinc.service_tickets?.some((ticket: any) => ticket.status !== 'tamamlandi');
 
                 return (vinc.lat && vinc.lng) && (
                     <Marker 
                         key={vinc.id} 
                         position={[vinc.lat, vinc.lng]} 
-                        // Arıza varsa Kırmızı, yoksa Yeşil ikon
+                        // Duruma göre ikon seçimi
                         icon={arizaVar ? redPulseIcon : greenIcon} 
                         eventHandlers={{
                             click: () => setSecilenVinc(vinc),
