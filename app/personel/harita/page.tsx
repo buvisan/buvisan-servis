c63e99a1a556bcc626389d42bce7ca6fae2ae394
Buvisan Admin <eticaretbuvisan@gmail.com>
2026-02-09 17:44:19 +0300

    Personel Ekranı

--- app/personel/harita/page.tsx
+++ app/personel/harita/page.tsx
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic';
 import { Loader2 } from 'lucide-react';
 
 // Haritayı dinamik olarak import ediyoruz ve Sunucu Taraflı Render (SSR)'ı kapatıyoruz.
-const HaritaBileseni = dynamic(() => import('@/components/HaritaBileseni'), {
+const HaritaBileseni2 = dynamic(() => import('@/components/HaritaBileseni'), {
   ssr: false, // 🔥 İŞTE SİHİRLİ KOD BURASI!
   loading: () => (
     <div className="h-screen flex items-center justify-center bg-slate-50">
@@ -18,7 +18,7 @@ const HaritaBileseni = dynamic(() => import('@/components/HaritaBileseni'), {
 export default function HaritaSayfasi() {
   return (
     <div className="h-screen w-full">
-      <HaritaBileseni />
+      <HaritaBileseni2 />
     </div>
   );
 }
